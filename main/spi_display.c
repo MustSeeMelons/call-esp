424c6b39d1eeb1fa357e44f8fdae565af11f1f4f
Strautins <strautins.janis@gmail.com>
2025-12-21 22:20:15 +0200

    Update SPI pins

--- main/spi_display.c
+++ main/spi_display.c
@@ -1,10 +1,8 @@
 #include "include/spi_display.h"
 
-#define PIN_NUM_MOSI 23
-#define PIN_NUM_MISO 19
-#define PIN_NUM_CLK  18
-#define PIN_NUM_CS   5
+#define MOSI 23
+#define CLK  18
+#define RCK  4 // Latch
 
 esp_err_t init_spi_display() {
-
 }
